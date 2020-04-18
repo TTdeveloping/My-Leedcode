6e6ae742215491a331a82d12970f204b62588db0
TTdeveloping <171593733@qq.com>
2020-04-18 20:33:54 +0800

    process...

---
+++ 83_DeleteDuplicates/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "tree.h"
+#include "linklist.h"
+#include "solution.h"
+
+
+int main() {
+	Solution s;
+
+	vector<int> nums = { 1, 1, 2, 3, 3 };
+	ListNode * l = creatLinkedList(nums.begin(), nums.end());
+	
+	ListNode * res = s.deleteDuplicates(l);
+
+	std::cout << "" << std::endl;
+
+	std::cout << std::endl <<  "Test Finished." << std::endl;
+	std::getchar();
+
+	return 0;
+} 
---
+++ 83_DeleteDuplicates/solution.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <iostream>
+//#include <cstdlib>
+#include <string>
+using std::string;
+#include <unordered_map>
+using std::unordered_map;
+#include <algorithm>
+#include <vector>
+using std::vector;
+#include <bitset>
+#include <array>
+using std::array;
+#include <unordered_set>
+#include <stack>
+using std::stack;
+using std::swap;
+
+/**
+	No 21：合并两个有序链表
+**/
+
+class Solution 
+{
+public:
+	ListNode *deleteDuplicates(ListNode* head)
+	{
+		ListNode *p = head;
+		while (p != NULL && p->next != NULL)
+		{
+			if (p->next->val == p->val)
+			{
+				ListNode *q = p->next;
+				p->next = q->next;
+			}
+			else
+			{
+				p = p->next;
+			}
+		}
+		return head;
+	}
+		
+};
