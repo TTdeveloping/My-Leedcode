b96e0f8eec849ee709881bb13b5b352c240923d1
TTdeveloping <171593733@qq.com>
2020-04-19 12:45:56 +0800

    kthToLast

---
+++ Interview_02_02-kthToLast/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "tree.h"
+#include "linklist.h"
+#include "solution.h"
+
+
+int main() {
+	Solution s;
+
+	vector<int> nums = { 1, 2, 3, 4, 5 };
+	int k = 3;
+	ListNode * l = creatLinkedList(nums.begin(), nums.end());
+	
+	int res = s.kthToLast(l,k);
+
+	std::cout << "" << std::endl;
+
+	std::cout << std::endl <<  "Test Finished." << std::endl;
+	std::getchar();
+
+	return 0;
+} 
---
+++ Interview_02_02-kthToLast/solution.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <iostream>
+//#include <cstdlib>
+#include <string>
+using std::string;
+#include <unordered_map>
+using std::unordered_map;
+#include <algorithm>
+#include <vector>
+using std::vector;
+#include <bitset>
+#include <array>
+using std::array;
+#include <unordered_set>
+#include <stack>
+using std::stack;
+using std::swap;
+
+/**
+	No 21：合并两个有序链表
+**/
+
+class Solution 
+{
+public:
+	int kthToLast(ListNode* head, int k)
+	{
+		if (head == NULL) return NULL;
+		ListNode *p = head;
+		ListNode *q = head;
+		int count = 1;
+		while (p->next != NULL)
+		{
+			count += 1;
+			p = p->next;
+
+		}
+		while (k != count)
+		{
+			q = q->next;
+			count--;
+		}
+		return q->val;
+
+	}
+		
+};
