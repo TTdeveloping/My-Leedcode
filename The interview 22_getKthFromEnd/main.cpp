a5cc4670bb6346573e5ce93a7310b884aa82d937
TTdeveloping <171593733@qq.com>
2020-06-06 22:52:41 +0800

    2020/6/6

---
+++ The interview 22_getKthFromEnd/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "tree.h"
+#include "linklist.h"
+#include "solution.h"
+
+
+int main() {
+	Solution s;
+
+	vector<int> nums = { 1, 2, 3, 4, 5 };
+	int k = 2;
+	ListNode * l = creatLinkedList(nums.begin(), nums.end());
+	ListNode * res = s.getKthFromEnd(l,k);
+	while (res!= NULL)
+	{
+		std::cout << res->val << std::endl;
+		res = res->next;
+	}
+
+	std::cout << "" << std::endl;
+
+	std::cout << std::endl <<  "Test Finished." << std::endl;
+	std::getchar();
+
+	return 0;
+} 
---
+++ The interview 22_getKthFromEnd/solution.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <iostream>
+//#include <cstdlib>
+#include <string>
+using std::string;
+#include <unordered_map>
+using std::unordered_map;
+#include <algorithm>
+#include <vector>
+using std::vector;
+#include <bitset>
+#include <array>
+using std::array;
+#include <unordered_set>
+#include <stack>
+using std::stack;
+using std::swap;
+
+/**
+	No 21：合并两个有序链表
+**/
+
+class Solution {
+public:
+	ListNode* getKthFromEnd(ListNode* head, int k)
+	{
+		//ListNode *p, *q;
+		ListNode *p;
+		p = head;
+		for (int i = 0; i < k; i++)
+		{
+			p = p->next;
+		}
+		//q = p;
+		while (p != NULL)
+		{
+			//q = q->next;
+			p = p->next;
+			head = head->next;
+		}
+
+		return head;
+
+	}
+};
