1e6818cf109f6781811ffa88c83982cb52f5ce47
TTdeveloping <171593733@qq.com>
2020-06-09 11:18:19 +0800

    2020/6/9

---
+++ 107_levelOrderBottom/solution.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <iostream>
+# include <queue>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	vector<vector<int>> levelOrderBottom(TreeNode* root) 
+	{
+		vector<vector<int>> res;
+		queue<TreeNode *> q;
+		if (root == NULL) return res;
+		q.push(root);
+		while (!q.empty())
+		{
+			int tep_size = q.size();
+			vector<int> temp;
+			for (int i = 1; i <= tep_size; i++)
+			{
+				TreeNode *p = q.front();
+				q.pop();
+				temp.push_back(p->val);
+				if (p->left != NULL) q.push(p->left);
+				if (p->right != NULL) q.push(p->right);
+			}
+			res.push_back(temp);
+
+		}
+		reverse(res.begin(), res.end());
+		return res;
+	}
+};
