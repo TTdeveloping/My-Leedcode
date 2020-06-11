97606f1bc3695428ccc1a84c9fda8ce1b51519aa
TTdeveloping <171593733@qq.com>
2020-06-11 15:34:33 +0800

    2020/6/10

--- 103_zigzagLevelOrder/solution.h
+++ 103_zigzagLevelOrder/solution.h
@@ -4,31 +4,37 @@
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
-	vector<int> preorderTraversal(TreeNode* root) {
-		vector<int> node_val;
-		queue<TreeNode*> q;
-		if (root == NULL) retrun node_val;
+	vector<vector<int>> zigzagLevelOrder(TreeNode* root)
+	{
+		vector<vector<int>> res;
+		queue<TreeNode *> q;
+		if (root == NULL) return res;
 		q.push(root);
+		int levl = 1;
 		while (!q.empty())
 		{
-			int len = q.size();
-			for (int i = 1; i <= len; i++)
+			int tep_size = q.size();
+			vector<int> temp;
+			for (int i = 1; i <= tep_size; i++)
 			{
 				TreeNode *p = q.front();
-				node_val.push_back(p->val)
-			}
-		}
+				q.pop();
+				temp.push_back(p->val);
+				if (p->left != NULL) q.push(p->left);
+				if (p->right != NULL) q.push(p->right);
 
+			}
+			if (levl % 2 == 0)
+			{
+				reverse(temp.begin(), temp.end());
+			}
+			res.push_back(temp);
+			levl++;
+
+		}
+		return res;
 	}
 };
