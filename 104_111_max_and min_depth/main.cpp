a5c1c7c0cbfc512e3a4b63eaa0b182eb3eec566e
TTdeveloping <171593733@qq.com>
2020-06-14 13:20:18 +0800

    2020/6/14

---
+++ 104_111_max_and min_depth/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "tree.h"
+#include "linklist.h"
+#include "solution.h"
+
+
+int main() {
+	Solution s;
+
+	const int N = 7;
+	int nums[N] = { 3,9,20,NULL, NULL,15,7};
+	//int nums[4] = { 1,4,3,2 };
+	TreeNode *tree;
+	creatTreeNode(tree, nums, N, 0);
+	//int res = s.maxDepth(tree);
+	int res =  s.minDepth(tree);
+	
+	std::cout << res << std::endl;
+	
+	std::cout <<  "Test Finished." << std::endl;
+	std::getchar();
+
+	return 0;
+}
---
+++ 104_111_max_and min_depth/solution.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include <iostream>
+# include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution1 {
+public:
+	int maxDepth(TreeNode* root) {
+		int count = 0;
+		queue<TreeNode*> q;
+		if (!root) return 0;
+		q.push(root);
+		while (!q.empty()) {
+			int len = q.size();
+			for (int i = 1; i <= len; i++) {
+				TreeNode *p = q.front();
+				q.pop();
+				if (p->left) q.push(p->left);
+				if (p->right) q.push(p->right);
+			}
+			count++;
+
+		}
+		return count;
+
+
+	}
+};
+class Solution {
+public:
+	int minDepth(TreeNode* root) {
+		int count = 0;
+		queue<TreeNode*> q;
+		if (!root) return 0;
+		q.push(root);
+		while (!q.empty()) {
+			int len = q.size();
+			count++;
+			for (int i = 1; i <= len; i++) {
+				TreeNode *p = q.front();
+				q.pop();
+				if ((p->left == NULL) && (p->right==NULL)) {
+					return count;
+				}
+				if (p->left != NULL) q.push(p->left);
+				if (p->right != NULL) q.push(p->right);
+			}
+		}
+		return count;
+	}
+};
