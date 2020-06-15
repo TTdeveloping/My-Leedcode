f2feb933e71c90f5576796ac7c7d5afd5a39e0fa
TTdeveloping <171593733@qq.com>
2020-06-15 17:40:21 +0800

    2020/6/15

---
+++ The interview55 - II_isBalanced/solution.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	bool isBalanced(TreeNode* root) {
+		if (root == NULL) return true;
+		if (abs(getHeight(root->left) - getHeight(root->right)) >1 ) return false;
+		return isBalanced(root->left) && isBalanced(root->right);
+	}
+	int getHeight(TreeNode* root) {
+		if (root == NULL) return 0;
+		int left = getHeight(root->left);
+		int right = getHeight(root->right);
+		return  left > right ? left + 1 : right +1;
+		
+
+	}
+
+	
+};
