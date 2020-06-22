94c0da1556f7c4593a011182d29ab08496658c6f
TTdeveloping <171593733@qq.com>
2020-06-22 11:17:14 +0800

    2020/6/22

---
+++ 7_reverse/main.cpp
@@ -0,0 +1,10 @@
+#include"solution.h"
+int main() {
+	Solution s;
+	int x = 123456789;
+	auto result = s.reverse(x);
+	cout << result << endl;
+	cout << "Test final" << endl;
+	getchar();
+	return 0;
+}
---
+++ 7_reverse/solution.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<unordered_map>
+#include <math.h>
+using namespace std;
+
+class Solution {
+public:
+	int reverse(int x) {
+		int res = 0;
+		while (x != 0) {
+			if (res<INT_MIN /10|| res>INT_MAX/10) return 0;
+			res = res * 10 + x % 10;
+			x /= 10;
+		}
+		return res;
+	}
+};
